1be263b091460e3e0d62b752e48d61c187bf7401
hzc11 <2548494221@qq.com>
2020-07-17 09:05:40 +0800

    你好：2020年7月16日  建立一个输出语句2131

--- cangku/src/hzc/Test.java
+++ cangku/src/hzc/Test.java
@@ -3,6 +3,6 @@ package hzc;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Hello world23");
+        System.out.println("Hello world2");
     }
 }
